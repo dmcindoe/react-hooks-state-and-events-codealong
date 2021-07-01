59ffe21f1875f8bf133f27af9096db3b4dad5baf
Andrew McIndoe <amcindoe@powerhrg.com>
2021-07-01 15:45:45 -0400

    hooks stat & event Code Along

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react"
 
-function Toggle() {
-  return <button>OFF</button>;
-}
+const Toggle = () => {
+  const [isOn, setIsOn] = useState(false);
 
-export default Toggle;
+  const handleClick = () => {
+    setIsOn((isOn) => !isOn);
+  }
+
+  const color = isOn ? "red" : "white";
+
+  return (
+    <button style={{ background: color }} onClick={handleClick}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
+} 
+
+export default Toggle
